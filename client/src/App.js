9c041d1d48c0d8966f765f54bad3d4fd1d82e31b
Balara <balarajayaweera@gmail.com>
2022-05-23 22:28:35 +0530

    reg and reset pw routes added

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import './App.css';
-import Login from './components/auth/Login';
+import Login from './components/auth/login/Login';
+import Register from './components/auth/register/Register';
+import ResetPassword from './components/auth/resetpassword/ResetPassword';
 
 function App() {
   return (
@@ -8,6 +10,8 @@ function App() {
       <Router>
         <Routes>
           <Route path="login" element={<Login/>}/>
+          <Route path="register" element={<Register/>}/>
+          <Route path="resetpassword" element={<ResetPassword/>}/>
         </Routes>        
       </Router>
    
