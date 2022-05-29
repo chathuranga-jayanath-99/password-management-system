4fb95f5ce2a33512ca542fcd694907350135f17f
chathuranga-jayanath-99 <85400922+chathuranga-jayanath-99@users.noreply.github.com>
2022-05-29 17:48:00 +0530

    deleted some unnecessary files

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,25 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import logo from './logo.svg';
-import './App.css';
-import Login from './components/auth/login/Login';
-import Register from './components/auth/register/Register';
-import ResetPassword from './components/auth/resetpassword/ResetPassword';
-import Password from './components/password';
-import NoPage from './components/nopage/NoPage';
-import Welcome from './components/welcome/Welcome';
-import PasswordManager from './components/passwordmanager/PasswordManager';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import logo from "./logo.svg";
+import "./App.css";
+import Login from "./components/auth/login/Login";
+import Register from "./components/auth/register/Register";
+import ResetPassword from "./components/auth/resetpassword/ResetPassword";
+import NoPage from "./components/nopage/NoPage";
+import Welcome from "./components/welcome/Welcome";
+import PasswordManager from "./components/passwordmanager/PasswordManager";
 
 function App() {
   return (
-  
-      <Router>
-        <Routes>
-          <Route index element={<Welcome/>}/>
-          <Route path="login" element={<Login/>}/>
-          <Route path="register" element={<Register/>}/>
-          <Route path="resetpassword" element={<ResetPassword/>}/>
-          <Route path="password" element={<Password/>}/>
-          <Route path="passwordmanager" element={<PasswordManager/>}/>
-          <Route path="*" element={<NoPage/>}/>
-        </Routes>        
-      </Router>
-   
-   
+    <Router>
+      <Routes>
+        <Route index element={<Welcome />} />
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+        <Route path="resetpassword" element={<ResetPassword />} />
+        <Route path="passwordmanager" element={<PasswordManager />} />
+        <Route path="*" element={<NoPage />} />
+      </Routes>
+    </Router>
   );
 }
 
