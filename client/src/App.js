b75aa588f1a1d46530462b9f919dd280cc90810f
Sivakajan-tech <51718908+Sivakajan-tech@users.noreply.github.com>
2022-05-29 10:04:49 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,24 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import logo from './logo.svg';
 import './App.css';
-import Login from './components/auth/Login';
-import Welcome from './components/auth/welcomepage';
-import PasswordManager from './components/passwordManager';
+import Login from './components/auth/login/Login';
+import Register from './components/auth/register/Register';
+import ResetPassword from './components/auth/resetpassword/ResetPassword';
+import Password from './components/password';
 
 function App() {
   return (
-
-    <Router>
-      <Routes>
-        <Route path="login" element={<Login />} />
-        <Route path="welcome" element={<Welcome />} />
-        <Route path="passwordmanager" element={<PasswordManager />} />
-      </Routes>
-    </Router>
-
+  
+      <Router>
+        <Routes>
+          <Route path="login" element={<Login/>}/>
+          <Route path="register" element={<Register/>}/>
+          <Route path="resetpassword" element={<ResetPassword/>}/>
+          <Route path="password" element={<Password/>}/>
+        </Routes>        
+      </Router>
+   
+   
   );
 }
 
