4f5d4ca755c0dca387d9e32f2e7ac56319aca7ad
chathuranga-jayanath-99 <85400922+chathuranga-jayanath-99@users.noreply.github.com>
2022-06-03 19:32:12 +0530

    reverted back to the prev working commit

--- client/src/App.js
+++ client/src/App.js
@@ -10,27 +10,6 @@ import PasswordManager from "./components/passwordmanager/PasswordManager";
 import { AuthProvider, RequireAuth} from "./context/AuthProvider";
 import Dashboard from "./components/dashboard/dashboard";
 
-<<<<<<< HEAD
-function App() {
-  return (
-    
-      <BrowserRouter>
-        <AuthProvider>
-          <Routes>
-            <Route index element={<Welcome />} />
-            <Route path="login" element={<Login />} />
-            <Route path="logout" element={<Logout />} />
-            <Route path="register" element={<Register />} />
-            <Route path="resetpassword" element={<ResetPassword />} />
-            <Route path="passwordmanager" element={<RequireAuth><PasswordManager /></RequireAuth>} />
-            <Route path="dashboard" element={<RequireAuth><Dashboard /></RequireAuth>} />
-            <Route path="*" element={<NoPage />} />
-          </Routes>
-        </AuthProvider>
-      </BrowserRouter>
-    
-  );
-=======
 class App extends Component {
   state = {};
 
@@ -62,7 +41,6 @@ class App extends Component {
       </Router>
     );
   }
->>>>>>> parent of 0338c6a (login and logout working.. tested code is commented.. check to get an idea)
 }
 
 export default App;
